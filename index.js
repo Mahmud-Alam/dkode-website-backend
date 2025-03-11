e150fd27af181faa8c4a03224a2e7f544dfff602
mahmud-alam <official.mahmud23@gmail.com>
2025-03-11 11:34:45 +0600

    add cors

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/db");
 
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
 
+// Connect to MongoDB
 connectDB();
 
 const port = process.env.PORT || 5000;
