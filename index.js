d3dca2dea7c9a9b78f90335578d2972825fd2d50
mahmud-alam <official.mahmud23@gmail.com>
2025-03-11 10:50:12 +0600

    connect to local mongodb database

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./config/db");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
 
+connectDB();
+
+const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`Server is running on the port: ${port}`);
 });
